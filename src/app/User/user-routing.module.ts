c7d2c82458af9ca693d70371ea5daa57ffaad77a
Waheedpaen <waheedpaen@gmail.com>
2022-07-01 09:17:57 +0500

    user Routing

--- src/app/User/user-routing.module.ts
+++ src/app/User/user-routing.module.ts
@@ -1,5 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbDatepickerModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { SharedModule } from '../Shared/common-module/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { UserTemplateComponent } from './user-template/user-template.component';
+import { AdminLayoutComponent } from '../admin/admin-template/admin-layout/admin-layout.component';
+import { AdminComponent } from '../admin/admin/admin.component';
+import { UserAdminComponent } from './user-admin/user-admin.component';
+import { UserLayoutComponent } from './user-template/user-layout/user-layout.component';
 
 
 
@@ -10,3 +21,27 @@ import { CommonModule } from '@angular/common';
   ]
 })
 export class UserRoutingModule { }
+export const AllCommonModule = [
+ UserRoutingModule,
+  NgbModule,
+  NgxSpinnerModule,
+  NgxDatatableModule,
+  NgxDatatableModule ,
+  SharedModule,
+  HttpClientModule,
+  NgbDatepickerModule,
+  NgSelectModule
+]
+export const allComponent = [
+
+
+  UserTemplateComponent,
+ UserLayoutComponent,
+  UserAdminComponent
+
+
+
+];
+export const allServices  = [
+
+]
--- src/app/User/user.module.ts
+++ src/app/User/user.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AllCommonModule, allComponent, allServices } from './user-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  declarations: [allComponent ],
+  imports: [AllCommonModule],
+  providers:[
+    allServices
   ]
 })
 export class UserModule { }
